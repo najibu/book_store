9ea7b28f342b29559694aa26521f4848c82fd147
najibu <ns.najibu@gmail.com>
2017-01-27 11:49:20 +0000

    Added users table

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'admin'
     ];
 
     /**
@@ -26,4 +26,34 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+   * Get the unique identifier for the user.
+   *
+   * @return mixed
+   */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+   * Get the password for the user.
+   *
+   * @return string
+   */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+   * Get the e-mail address where password reminders are sent.
+   *
+   * @return string
+   */
+    public function getReminderEmail()
+    {
+        return $this->email;
+    }
 }
